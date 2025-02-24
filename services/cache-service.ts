f898e5117e34a1bbb84978107ae59fbce8d1d979
iyifr <adekileiyimide@gmail.com>
2025-02-24 14:24:10 +0100

    feat: fix bentocache issue

--- services/cache-service.ts
+++ services/cache-service.ts
@@ -1,16 +1,22 @@
-// import { BentoCache, bentostore } from 'bentocache'
-// import { memoryDriver } from 'bentocache/drivers/memory'
+import { BentoCache, bentostore } from 'bentocache'
+import { memoryDriver } from 'bentocache/drivers/memory'
 
-// export const bento = new BentoCache({
-//     default: 'myCache',
-//     stores: {
-//         // A first cache store named "myCache" using 
-//         // only L1 in-memory cache
-//         myCache: bentostore()
-//             .useL1Layer(memoryDriver({ maxSize: '100mb' })),
-//     },
-//     plugins: []
-// })
+export const bento = new BentoCache({
+    default: 'dataCache',
+    stores: {
+        dataCache: bentostore()
+            .useL1Layer(memoryDriver({ maxSize: '100mb' })),
+    },
+})
 
-// export const UserCache = bento.namespace('users')
-// export const AppCache = bento.namespace('apps')
+bento.on('cache:hit', ({ key, value, store }) => {
+    console.log(`cache:hit: ${key}, value ${value}, store ${store}`)
+})
+
+bento.on('cache:miss', ({ key, store }) => {
+    console.log(`cache:miss: ${key}, store ${store}`)
+})
+
+export const UserCache = bento.namespace('users')
+export const AppCache = bento.namespace('apps')
+export const KeyCache = bento.namespace('keys')
